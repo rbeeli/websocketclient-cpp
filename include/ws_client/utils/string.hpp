ff54260d2bc8914f25077bfc1594f8f7a88b7e72
Rino Beeli <40661605+rbeeli@users.noreply.github.com>
2024-09-09 22:25:00 +0300

    inline string_like_hash functions

--- include/ws_client/utils/string.hpp
+++ include/ws_client/utils/string.hpp
@@ -115,15 +115,15 @@ inline string string_from_bytes(span<byte> data) noexcept
 struct string_like_hash
 {
     using is_transparent = void;
-    [[nodiscard]] size_t operator()(const char* txt) const
+    [[nodiscard]] inline size_t operator()(const char* txt) const
     {
         return std::hash<std::string_view>{}(txt);
     }
-    [[nodiscard]] size_t operator()(std::string_view txt) const
+    [[nodiscard]] inline size_t operator()(std::string_view txt) const
     {
         return std::hash<std::string_view>{}(txt);
     }
-    [[nodiscard]] size_t operator()(const std::string& txt) const
+    [[nodiscard]] inline size_t operator()(const std::string& txt) const
     {
         return std::hash<std::string>{}(txt);
     }
